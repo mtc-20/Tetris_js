9a251008f69a6a9a5a1a7a17b028f90d241c5123
inkwolf_20 <mamenthomas.c@gmail.com>
2020-06-14 16:06:10 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# Tetris_js
-Basic Tetris on Javascript
+# BASIC TETRIS
+**A simple Tetris game made in Javascript based on the tutorial by Ania Kubow [@kubowania](https://github.com/kubowania) for freecodecamp. This is my first ~~project~~ time with both javascript and CSS, and I think this was a good introduction.**
+
+- The link to the article can be found [here](https://www.freecodecamp.org/news/learn-javascript-by-creating-a-tetris-game/).
+
+- This is the link to the [YouTube video](https://www.youtube.com/watch?v=rAUn1Lom6dw)
+
+<p align="right"> <img src='LogoMakr_0nVUmr.png' width='75px'> </p> <p align="right"><em>Test Logo</em> </p>
+
+**CONTENTS**
+- [Usage](#usage)
+- [Basic Controls](#basic-controls)
+- [Changes and Future Improvements](#changes)
+- [Resources and Credits](#resources)
+
+<p align="center"> <img src='screen.png'> </p> <p align="center"><em>Sample</em> </p>
+
+## Usage / INSTALLATION
+Just open the `html` file and you should be able to play the game
+>**NOTE:** This has only been tested on Windows 10 desktop browsers at 100% zoom (the graphics go wonky at lower zooms!)
+
+### Preview
+---
+> Will add link to preview HTML once I figure out how
+---
+
+## Basic Controls
+- Arrow keys to control tetromino. <kbd>Up</kbd> or <kbd>w</kbd> to rotate tetromino
+- <kbd>p</kbd> to pause/resume game
+- <kbd>m</kbd> to toggle music
+
+## Changes
+- Added background music that can be paused
+- Introduced some web fonts and 
+- Changed the layout to have a big top-centred title and all the buttons together on the left 
+- Added code to increase the speed of falling tetrominoes at regular intervals
+- [ ] Layout to be improved further
+    - [ ] Try and implement responsive (more CSS)
+    - [ ] Credits/About page
+- [ ] **Implement a working <kbd>Restart</kbd> button**
+ - At the moment cannot figure out how to 'refresh' the grid without affecting the hidden (*taken*) row
+- [ ] Introduce concept of levels or difficulty
+
+## Resources
+- **Fonts:** [Abril Fatface](https://fonts.google.com/specimen/Abril+Fatface), [Press Play 2P](https://fonts.google.com/specimen/Press+Start+2P?category=Serif,Sans+Serif,Display,Monospace#pairings) and others from *Google fonts*
+- **Icon(s):** *[Font Awesome](https://fontawesome.com/start)*
+- **Music:** [Mind Bender](https://soundimage.org/puzzle-music/) - Puzzle Music from *soundimage*
+- **Logo:** Free logo created at [Logomakr](https://logomakr.com/) and converted to `.ico` using [prodraw](http://www.prodraw.net/favicon/generator.php)
+- [CSS Cheatsheet](https://www.w3schools.com/css/default.asp)
---
+++ app.js
@@ -0,0 +1,326 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div')) 
+    const scoreDisplay = document.querySelector('#score')
+
+    // Buttons
+    const startBtn = document.querySelector('#start-button')
+    const restartBtn = document.querySelector('#restart-button')
+    const audioIcon = document.querySelector('#audio-icon')
+
+    // Audio
+    const bgm = document.getElementById('bgm')
+    bgm.loop = true
+    bgm.status = true
+
+    const width = 10
+    let nextRandom = 0
+    let timerID
+    let score = 000
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
+
+    let timePeriod = 500
+    let point = 10
+    paused = false
+
+    // Tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const oTetromino = [
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1],
+        [0, 1, width, width+1]
+]
+
+    const tTetromino = [
+        [0, 1, 2, width+1],
+        [width, 1, width+1, width*2+1],
+        [1, width, width+1, width+2],
+        [1, width+1, width*2+1, width+2]
+    ]
+
+    const zTetromino = [
+        [0, 1, width+1, width+2],
+        [2, width+1, width+2, width*2+1],
+        [0, 1, width+1, width+2],
+        [2, width+1, width+2, width*2+1]
+    ]
+
+    const iTetromino = [
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3],
+        [1, width+1, width*2+1, width*3+1],
+        [width, width+1, width+2, width+3]
+    ]
+
+    const theTetrominos = [lTetromino,iTetromino,zTetromino,oTetromino,tTetromino]
+
+    // Set variables
+    let currentPosition = 3
+    let currentRotation = 0
+
+    // Randomly select a tetromino
+    let random = Math.floor(Math.random()*theTetrominos.length)
+    let current = theTetrominos[random][currentRotation]
+
+    // Function to draw Tetromino
+    function draw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+            squares[currentPosition + index].style.backgroundColor = colors[random]
+        })
+    }
+    //draw()
+    
+    // Undraw Tetrommino
+    function undraw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+            squares[currentPosition + index].style.backgroundColor = ''
+        })
+    }
+
+    // Move Tetromino down every second
+    // timerID = setInterval(moveDown, 500)
+
+    // Assign functions to keyCodes
+    function control(e){
+        if(e.keyCode === 37){
+            moveLeft()
+        }
+        else if(e.keyCode === 39){
+            moveRight()
+        }
+        else if(e.keyCode === 40){
+            moveDown()
+        }
+        else if(e.keyCode === 38 || e.keyCode === 87){
+            rotate()
+        }
+        else if(e.keyCode === 80){
+            if(!paused){
+                pauseGame()
+                paused = true
+            }
+            else{
+                paused = false
+                playGame()
+            }
+        }
+        else if(e.keyCode === 77){
+            toggleMusic()
+        }
+    }
+    document.addEventListener('keyup', control)
+    
+
+
+    function toggleMusic(){
+        if(bgm.status){
+            bgm.pause()
+            bgm.status = false
+        }
+        else {
+            bgm.play()
+            bgm.status = true
+        }
+    }
+
+
+    function moveDown(){
+        undraw()
+        currentPosition += width
+        draw()
+        freeze()
+        
+    }
+
+    //Freeze
+    function freeze(){
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
+            current.forEach(index => {
+                squares[currentPosition + index].classList.add('taken')
+                squares[currentPosition + index].style.backgroundImage = 'linear-gradient(rgba(0,0,0,0), rgb(255,255,255))'
+            })
+
+            // Start new Tetromino
+            // console.log('Starting new')
+            random = nextRandom
+            nextRandom = Math.floor(Math.random()*theTetrominos.length)
+            current = theTetrominos[random][currentRotation]
+            currentPosition = 4
+            currentRotation = 0
+            draw()
+            displayShape()
+            addScore()
+            gameOver()
+        }
+    }
+
+    // Move Tetromino left within grid
+    function moveLeft(){
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index)%width === 0)
+
+        if (!isAtLeftEdge) currentPosition -= 1
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+        draw()
+    }
+
+    // Move Tetromino right within grid
+    function moveRight(){
+        undraw()
+        const isAtRightEdge = current.some(index => (currentPosition + index)%width === width-1)
+
+        if (!isAtRightEdge) currentPosition += 1
+
+        if (current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition -=1
+        }
+        draw()
+    }
+
+    // Rotate Tetromino within grid
+    function rotate(){
+        undraw()
+        currentRotation++
+        if (currentRotation === current.length){
+            currentRotation = 0
+        }
+        current = theTetrominos[random][currentRotation]
+        draw()
+    }
+
+    // Display next Tetromino
+    const displaySquares = document.querySelectorAll('.mini-grid div')
+    const displayWidth = 4
+    const displayIndex = 0
+    const upNextTetrominos = [
+        [1, displayWidth+1, displayWidth*2+1, 2], //l
+        [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1], //i
+        [0, 1, displayWidth+1, displayWidth+2], //z
+        [0, 1, displayWidth, displayWidth+1], //o
+        [0, 1, 2, displayWidth+1] //t
+    ]
+
+    function displayShape(){
+        // Remove any trace of tetromino from grid
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+            square.style.backgroundColor = ''
+        })
+
+        // console.log('Displaying next')
+
+        upNextTetrominos[nextRandom].forEach(index => {
+            displaySquares[displayIndex + index].classList.add('tetromino')
+            displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
+        })
+    }
+
+    // Add functionality to button
+    startBtn.addEventListener('click', () => {
+        if (timerID){
+            pauseGame()
+            // bgm.pause()
+            // clearInterval(timerID)
+            // timerID = null
+        }
+        else { 
+            playGame()
+        }
+    })
+
+    function pauseGame(){
+        bgm.pause()
+        clearInterval(timerID)
+        timerID = null
+    }
+
+    function playGame(){
+        if(bgm.status){
+            bgm.play()
+        }
+        draw()
+        timerID = setInterval(moveDown, timePeriod)
+        nextRandom = Math.floor(Math.random()*theTetrominos.length)
+        displayShape()
+    }
+
+    restartBtn.addEventListener('click', restart())
+    audioIcon.addEventListener('click', () => toggleMusic())
+
+    function clicky(){console.log('icon clciked')}
+
+
+    // TODO: Design a Restart state
+    function restart(){
+
+
+
+
+        score = 0
+        currentPosition = 4
+        currentRotation = 0
+    }
+    
+ 
+
+    // Add score
+    function addScore(){
+        for(let i=0; i < 199; i+= width){
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+            // console.log('Checking score')
+            if(row.every(index => squares[index].classList.contains('taken'))){
+                // console.log('scored')
+                score += point
+                scoreDisplay.innerHTML = score
+                row.forEach(index => {
+                    squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
+                    squares[index].style.backgroundColor = ''
+                    squares[index].style.backgroundImage = ''
+                })
+                const squaresRemoved = squares.splice(i, width)
+                // console.log(squaresRemoved)
+                squares = squaresRemoved.concat(squares)
+                squares.forEach(cell => grid.appendChild(cell))
+            }
+        }
+
+        if (score%50 === 0 && score > 0 ){
+            timePeriod -= 20
+            console.log('Speed bumped! '+timePeriod)
+        }
+    }
+
+    // Game over state
+    function gameOver(){
+        if(current.some(index => squares[currentPosition+index].classList.contains('taken'))){
+            scoreDisplay.innerHTML = 'end'
+            clearInterval(timerID)
+            bgm.pause()
+            alert('Game Over! \n Score: '+score)
+            
+
+        }
+    }
+
+    
+    //console.log(theTetrominos)
+})
